952a2484e3f825a1a611261db7e520fb9eec3721
S. Roland Marshall <samuel.r.marshall@gmail.com>
2022-01-13 23:06:26 -0500

    added more styling

--- app/models/bingo_card.rb
+++ app/models/bingo_card.rb
@@ -1,9 +1,9 @@
 class BingoCard < ApplicationRecord
   has_and_belongs_to_many :bingo_numbers
-  
+
   def pick_numbers
     %w(b i n g o).each do |letter|
-      BingoNumber.where(letter: letter).sample(5).each do |number|
+      BingoNumber.where(letter: letter).sample(5).sort_by(&:number).each do |number|
         number.bingo_cards << self
       end
     end
